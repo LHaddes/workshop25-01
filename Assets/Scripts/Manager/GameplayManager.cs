27df2d8453e54363ac12719e167dd5e96d971962
sam <samuel.brb19@gmail.com>
2021-01-25 15:40:42 +0100

    GM avancé

--- Assets/Scripts/Manager/GameplayManager.cs
+++ Assets/Scripts/Manager/GameplayManager.cs
@@ -1,20 +1,23 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
+﻿using TMPro;
 using UnityEngine;
+
 
 public class GameplayManager : MonoBehaviour
 {
+    [Header("Text")]
     public TextMeshProUGUI nbEnemisText;
     public TextMeshProUGUI nbMoneyText;
+    public TextMeshProUGUI timerText;
 
     public Transform[] posForMagicBoxes;
+
+    public GameObject MagicBox;
     
     public int nbMoneyPlayer;
     public int nbWave;
 
     public float countdownBetweenTwoWaves;
+    public float timer;
     
     #region Singleton
 
@@ -36,17 +39,24 @@ public class GameplayManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
     }
     
     public void UpdateEnemis(int nbEnemis)
     {
         nbEnemisText.text = nbEnemis.ToString();
+        timerText.text = timer.ToString("F0");
     }
 
     public void UpdateMoney(int nbMoney)
     {
         nbMoneyPlayer += nbMoney;
         nbMoneyText.text = nbMoneyPlayer.ToString();
+    }
+
+    public void SetMagicBox()
+    {
+        MagicBox.transform.position = posForMagicBoxes[Random.Range(0, posForMagicBoxes.Length)].position;
+        MagicBox.SetActive(true);
     }
 }
