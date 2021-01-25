703ecc9b64ad8b2028c8bc8eaa04811d7a65683c
Kilian <haddesgaming@gmail.com>
2021-01-25 16:18:00 +0100

    Player Life

---
+++ Assets/Scripts/Player/PlayerLife.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLife : MonoBehaviour
+{
+    public int maxLifePoints;
+
+    public int currentLifePoints;
+
+    public bool isInvulnerable;
+    public float invincibilityBlink;
+    private float timer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentLifePoints = maxLifePoints;
+    }
+
+    public void Hurt(int damage)
+    {
+        if (!isInvulnerable)
+        {
+            currentLifePoints -= damage;
+            isInvulnerable = true;
+        }
+    }
+
+    public void Death()
+    {
+        //TODO GAME OVER
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isInvulnerable)
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= invincibilityBlink)
+            {
+                isInvulnerable = false;
+                timer = 0f;
+            }
+        }
+        
+        if (currentLifePoints <= 0)
+        {
+            Death();
+        }
+    }
+}
