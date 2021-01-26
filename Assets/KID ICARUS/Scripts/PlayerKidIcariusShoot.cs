5be8122ae596e65fbdb05e68b27c75663bf6c9e4
sam <samuel.brb19@gmail.com>
2021-01-26 14:07:06 +0100

    correction shoot

--- Assets/KID ICARUS/Scripts/PlayerKidIcariusShoot.cs
+++ Assets/KID ICARUS/Scripts/PlayerKidIcariusShoot.cs
@@ -5,7 +5,6 @@ public class PlayerKidIcariusShoot : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -13,44 +12,25 @@ public class PlayerKidIcariusShoot : MonoBehaviour
     {
         float movementX = GetComponent<PlayerKidIcariusMovement>().xMove;
         float movementY = GetComponent<PlayerKidIcariusMovement>().yMove;
-        Vector3 mousePosition = Input.mousePosition;
-        float mousePositionX = mousePosition.x;
-        float mousePositionY = mousePosition.y;
-        float playerPositionX = transform.position.x;
-        float playerPositionY = transform.position.y;
 
-        if (movementX > 0 && mousePositionX > playerPositionX)
+        if (movementX > 0)
         {
             //TODO le player va à droite et regarde à droite
         }
 
-        else if (movementX < 0 && mousePositionX > playerPositionX)
+        else if (movementX < 0)
         {
             //TODO le player va à gauche et regarde à droite
         }
-        else if (movementX > 0 && mousePositionX < playerPositionX)
-        {
-            //TODO le player va à droite et regarde à gauche
-        }
-        else if (movementX < 0 && mousePositionX < playerPositionX)
-        {
-            //TODO le player va à gauche et regarde à gauche
-        }
-        else if (movementY > 0 && mousePositionY > playerPositionY)
+
+        else if (movementY > 0)
         {
             //TODO le player va en haut et regarde en haut
         }
-        else if (movementY > 0 && mousePositionY < playerPositionY)
-        {
-            //TODO le player va en haut et regarde en bas
-        }
-        else if (movementY < 0 && mousePositionY > playerPositionY)
+
+        else if (movementY < 0)
         {
             //TODO le player va en bas et regarde en haut
-        }
-        else if (movementY < 0 && mousePositionY < playerPositionY)
-        {
-            //TODO le player va en bas et regarde en bas
         }
     }
 }
