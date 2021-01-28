04a9c4285acb342464a482fca9957abf49e46f44
Kilian <haddesgaming@gmail.com>
2021-01-28 14:40:53 +0100

    PauseMenu fonctionnel

---
+++ Assets/BO ZOMBIE/Scripts/Manager/PauseManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public bool isPaused;
+
+    public GameObject pauseMenu;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        isPaused = true;
+        Time.timeScale = 0f;
+        pauseMenu.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        isPaused = false;
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+    }
+
+    public void Menu()
+    {
+        //TODO Retour au menu principal
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
