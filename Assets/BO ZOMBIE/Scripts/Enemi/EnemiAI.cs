18364cef6876548034c385aa608e024b4f10bdaa
sam <samuel.brb19@gmail.com>
2021-01-29 09:29:22 +0100

    rotation des ennemis

--- Assets/BO ZOMBIE/Scripts/Enemi/EnemiAI.cs
+++ Assets/BO ZOMBIE/Scripts/Enemi/EnemiAI.cs
@@ -41,6 +41,9 @@ public class EnemiAI : MonoBehaviour
 
     private void Update()
     {
+        Vector3 dir = target.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         if (playerInRange)
         {
             _hitRate -= Time.deltaTime;
