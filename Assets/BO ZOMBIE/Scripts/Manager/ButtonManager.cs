99c636d79e8e26ea5d2941db6d81d2c8c568bfa7
sam <samuel.brb19@gmail.com>
2021-01-29 10:09:18 +0100

    scène menu ok

---
+++ Assets/BO ZOMBIE/Scripts/Manager/ButtonManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public GameObject panelCommands;
+    public void Play()
+    {
+        SceneManager.LoadScene("SceneProto");
+    }
+
+    public void Commands()
+    {
+        panelCommands.SetActive(true);
+    }
+
+    public void Back()
+    {
+        panelCommands.SetActive(false);
+    }
+    public void OnQuit()
+    {
+        Application.Quit();
+    }
+}
